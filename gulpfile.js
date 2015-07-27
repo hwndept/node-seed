a3b9f2c7de49027bfec8d287cecb71afff7cc9ef
Nikolay Shatalov <hwndept@gmail.com>
2015-07-28 01:37:01 +0300

    Initialized jshint task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 
-gulp.task('default', function () {
-  // place code for your default task here
+var GULP_FILE = ['gulpfile.js'];
+var SRC_FILES = ['src/**/*.js'];
+var TEST_FILES = ['test/**/*.js'];
+
+gulp.task('jshint', function () {
+  return gulp.src(GULP_FILE.concat(SRC_FILES, TEST_FILES))
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter("fail"));
 });
+
+gulp.task('default', ['jshint']);
