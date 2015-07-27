d43855464c75ec40a0e6e85d4dc2118e487fab1a
Nikolay Shatalov <hwndept@gmail.com>
2015-07-28 01:42:57 +0300

    Initialized jscs task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
 
 var GULP_FILE = ['gulpfile.js'];
 var SRC_FILES = ['src/**/*.js'];
@@ -12,4 +13,9 @@ gulp.task('jshint', function () {
     .pipe(jshint.reporter("fail"));
 });
 
-gulp.task('default', ['jshint']);
+gulp.task('jscs', function () {
+  return gulp.src(GULP_FILE.concat(SRC_FILES, TEST_FILES))
+    .pipe(jscs());
+});
+
+gulp.task('default', ['jshint', 'jscs']);
