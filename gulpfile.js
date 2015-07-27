c2defd1c48fd12470126f37f8a294cc6c907ae4f
Nikolay Shatalov <hwndept@gmail.com>
2015-07-28 02:10:22 +0300

    Initialized test task with code coverage checking

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
 
 var GULP_FILE = ['gulpfile.js'];
 var SRC_FILES = ['src/**/*.js'];
 var TEST_FILES = ['test/**/*.js'];
+var TEST_CASE_FILES = ['test/**/*.test.js'];
+var COVERAGE_REPORT_DIR = 'build/coverage';
 
 gulp.task('jshint', function () {
   return gulp.src(GULP_FILE.concat(SRC_FILES, TEST_FILES))
@@ -18,4 +22,24 @@ gulp.task('jscs', function () {
     .pipe(jscs());
 });
 
-gulp.task('default', ['jshint', 'jscs']);
+gulp.task('test', function () {
+  return gulp.src(SRC_FILES)
+    .pipe(istanbul({
+      includeUntested: true
+    }))
+    .pipe(istanbul.hookRequire())
+    .on('finish', function () {
+      gulp.src(TEST_CASE_FILES)
+        .pipe(mocha())
+        .pipe(istanbul.writeReports({
+          dir: COVERAGE_REPORT_DIR
+        }))
+        .pipe(istanbul.enforceThresholds({
+          thresholds: {
+            global: 100
+          }
+        }));
+    });
+});
+
+gulp.task('default', ['jshint', 'jscs', 'test']);
