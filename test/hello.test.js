7ffe1896cbe663b69bbc03381f979ef13612339d
Nikolay Shatalov <hwndept@gmail.com>
2015-08-07 01:33:40 +0300

    resolves #17 - Added jscs options

--- test/hello.test.js
+++ test/hello.test.js
@@ -4,11 +4,11 @@ var assert = require('assert');
 var hello = require('../src/hello');
 
 describe('hello.js', function () {
-    it('it should return "Hello, world!" when name is empty', function () {
-        assert.equal('Hello, world!', hello());
-    });
+  it('it should return "Hello, world!" when name is empty', function () {
+    assert.equal('Hello, world!', hello());
+  });
 
-    it('it should pass the name when it is passed', function () {
-        assert.equal('Hello, username!', hello('username'));
-    });
+  it('it should pass the name when it is passed', function () {
+    assert.equal('Hello, username!', hello('username'));
+  });
 });
