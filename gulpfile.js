7ed360511265332fd7e1d4e9e73465305a01617a
hwndept <hwndept@gmail.com>
2015-08-13 22:07:14 +0000

    resolves #33 - ES6 Support using Babel

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,17 @@ var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
 var jsdoc = require('gulp-jsdoc');
 var sequence = require('run-sequence');
+var babel = require('gulp-babel');
+var isparta = require('isparta');
+var babelRegister = require('babel/register');
 
 var GULP_FILE = ['gulpfile.js'];
 var SRC_FILES = ['src/**/*.js'];
 var TEST_FILES = ['test/**/*.js'];
 var TEST_CASE_FILES = ['test/**/*.test.js'];
 var COVERAGE_REPORT_DIR = 'build/coverage';
+var COMPILED_SRC_DIR = 'build/source';
+var COMPILED_SRC_FILES = [COMPILED_SRC_DIR + '/**/*.js'];
 var JSDOC_DIR = 'build/jsdoc';
 
 gulp.task('jshint', function (done) {
@@ -32,12 +37,13 @@ gulp.task('jscs', function (done) {
 gulp.task('test', function (done) {
   gulp.src(SRC_FILES)
     .pipe(istanbul({
+      instrumenter: isparta.Instrumenter,
       includeUntested: true
     }))
     .pipe(istanbul.hookRequire())
     .on('finish', function () {
       gulp.src(TEST_CASE_FILES)
-        .pipe(mocha())
+        .pipe(mocha({compilers: {js: babelRegister}}))
         .pipe(istanbul.writeReports({
           dir: COVERAGE_REPORT_DIR
         }))
@@ -50,14 +56,21 @@ gulp.task('test', function (done) {
     });
 });
 
-gulp.task('jsdoc', function (done) {
+gulp.task('compile', function (done) {
   gulp.src(SRC_FILES)
+    .pipe(babel())
+    .pipe(gulp.dest(COMPILED_SRC_DIR))
+    .on('finish', done);
+});
+
+gulp.task('jsdoc', ['compile'], function (done) {
+  gulp.src(COMPILED_SRC_FILES)
     .pipe(jsdoc(JSDOC_DIR))
     .on('finish', done);
 });
 
 gulp.task('build', function (done) {
-  sequence('jshint', 'jscs', 'test', 'jsdoc', done);
+  sequence('jshint', 'jscs', 'test', 'compile', 'jsdoc', done);
 });
 
 gulp.task('pre-commit', ['build']);
